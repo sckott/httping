3fc34a9c9eae811f0767adaaa933b8ba0dbdd148
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-02 23:30:10 -0800

    first commit

---
+++ R/httping-package.R
@@ -0,0 +1,9 @@
+#' Ping urls to time requests
+#'
+#' @name httping-package
+#' @aliases httping
+#' @docType package
+#' @title Ping urls to time requests
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @keywords package
+NULL
---
+++ R/time.R
@@ -0,0 +1,66 @@
+#' Ping a url to time the request
+#'
+#' @export
+#' @import httr
+#' @importFrom jsonlite fromJSON
+#' @importFrom pryr object_size
+#'
+#' @param .request A httr response object
+#' @param count integer, Number of requests to do.
+#' @param delay integer, Seconds to delay successive calls by.
+#' @param flood logical; If TRUE, no delay between requests. If FALSE, delay by 1 second.
+#' @param verbose logical; If TRUE, print progress.
+#' @param ... Further args passed on to functions in \code{httr}
+#' @examples \donttest{
+#' GET("http://localhost:9200") %>% time()
+#' GET("http://localhost:5984") %>% time()
+#' GET("http://google.com") %>% time()
+#' }
+
+time <- function(.request, count=10, delay = 1, flood = FALSE, verbose=TRUE, ...)
+{
+  stopifnot(is(.request, "response"))
+  if(verbose) cat(sprintf("%s kb - %s code:%s time(ms):%s", get_kb(.request), .request$url, .request$status_code, .request$times[["total"]]*1000), sep = "\n")
+  if(count > 1) count <- count - 1
+  reps <- replicate(count, rerequest_(.request, verbose), simplify = FALSE)
+  all <- do.call(c, list(list(.request), reps))
+  times <- pluck(all, "times")
+  nmz <- names(times[[1]])
+  avgs <- setNames(lapply(nmz, function(z){
+    tt <- pluck(times, z, double(1))
+    setNames(sapply(list(min, max, mean), function(x) calc(tt, x)), c("min","max","mean"))
+  }), nmz)
+  structure(list(times=times, averages=avgs, request=.request),
+            count = count,
+            delay = delay,
+            class="http_time")
+}
+
+calc <- function(x, fxn) format(fxn(x*1000, na.rm = TRUE), scientific = FALSE)
+
+rerequest_ <- function(x, verbose=TRUE){
+  tmp <- rerequest(x)
+  if(verbose) cat(sprintf("%s kb - %s code:%s time(ms):%s", get_kb(tmp), tmp$url, tmp$status_code, tmp$times[["total"]]*1000), sep = "\n")
+  return(tmp)
+}
+
+get_kb <- function(x) (pryr::object_size(x)/1000)[[1]]
+
+#' @export
+print.http_time <- function(x, ...){
+  cat("<http time>", sep = "\n")
+  cat(paste0("  Avg. min (ms):  ", x$averages[['total']][['min']]), sep = "\n")
+  cat(paste0("  Avg. max (ms):  ", x$averages[['total']][['max']]), sep = "\n")
+  cat(paste0("  Avg. mean (ms): ", x$averages[['total']][['mean']]), sep = "\n")
+}
+
+#' @export
+summary.http_time <- function(object, ...){
+  cat("<http time, summary>", sep = "\n")
+  cat(paste0("  Avg. total (s): ", object$averages[['total']]), sep = "\n")
+  cat(paste0("  Avg. redirect (s): ", object$averages[['redirect']]), sep = "\n")
+  cat(paste0("  Avg. namelookup time (s): ", object$averages[['namelookup']]), sep = "\n")
+  cat(paste0("  Avg. connect (s): ", object$averages[['connect']]), sep = "\n")
+  cat(paste0("  Avg. pretransfer (s): ", object$averages[['pretransfer']]), sep = "\n")
+  cat(paste0("  Avg. starttransfer (s): ", object$averages[['starttransfer']]), sep = "\n")
+}
